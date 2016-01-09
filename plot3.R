b4212043bd34511a42568d499e8983972e6840d3
Buel Chandler <buel@me.com>
2016-01-09 12:30:47 -0500

    make totally stand-alone

--- plot3.R
+++ plot3.R
@@ -1,9 +1,21 @@
-dataFile <- "household_power_consumption.txt" ## assume file in working directory
+### Note: wrtten as a stand-alone
+
+## get zip UCI file
+pkgs = c("downloader") ## use "downloader" package from CRAN
+if(length(new.pkgs <- setdiff(pkgs, rownames(installed.packages())))) install.packages(new.pkgs)
+library(downloader)
+
+download("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest="household_power_consumption.zip", mode="wb")
+unzip ("household_power_consumption.zip", exdir = "./")
+
+## now load the data.table
+dataFile <- "household_power_consumption.txt" 
 power <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, na.strings = "?")
 
 ## for this graph, we are only looking at 2007-02-01 and 2007-02-02
 power$Date <- as.Date(power$Date, format="%d/%m/%Y")
 subPower <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+
 ## clean up our data column we're interested in
 subPower$Sub_metering_1 <- as.numeric(as.character(subPower$Sub_metering_1))
 subPower$Sub_metering_2 <- as.numeric(as.character(subPower$Sub_metering_2))
--- plot4.R
+++ plot4.R
@@ -1,4 +1,15 @@
-dataFile <- "household_power_consumption.txt" ## assume file in working directory
+### Note: wrtten as a stand-alone
+
+## get zip UCI file
+pkgs = c("downloader") ## use "downloader" package from CRAN
+if(length(new.pkgs <- setdiff(pkgs, rownames(installed.packages())))) install.packages(new.pkgs)
+library(downloader)
+
+download("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", dest="household_power_consumption.zip", mode="wb")
+unzip ("household_power_consumption.zip", exdir = "./")
+
+## now load the data.table
+dataFile <- "household_power_consumption.txt" 
 power <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, na.strings = "?")
 
 ## for this graph, we are only looking at 2007-02-01 and 2007-02-02
